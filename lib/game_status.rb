510364de2e3c2638aa55cd9e30a31ba763485af3
Lucas Souza <souza.lucas01@gmail.com>
2020-03-14 00:25:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,30 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+[0,1,2], #Top_row
+[3,4,5], #Middle_row
+[6,7,8], #Bottom_row
+[0,3,6], #Vert_left
+[1,4,7], #Vert_middle
+[2,5,8], #Vert_right
+[0,4,8], #Diag_left
+[2,4,6], #Diag_right
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      return win_combination
+    end
+  end
+  return false
+end
