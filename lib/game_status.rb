d4ffe3ef6b86312e7531b6462aa733daa7882e37
Lucas Souza <souza.lucas01@gmail.com>
2020-03-14 01:19:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -48,3 +48,11 @@ def draw?(board)
     false
   end
 end
+
+#over?_method
+def over?(board)
+  if won?(board) == true || draw?(board) == true || full?(board) == true
+    return true    
+  end
+  
+end
